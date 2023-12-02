3f040dc587a54f8302a91005bda81f3c840fc8aa
Muhammad Hussain <imhussain9014@gmail.com>
2023-12-02 20:33:55 +0600

    Add BinarySearch algorithm

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class LinearSearch {
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        int target = 6;
+        int check = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i] == target){
+                check = 1;
+                break;
+            }
+        }
+        if (check == 1){
+            System.out.println("Found");
+        }else{
+            System.out.println("Not Found");
+        }
+    }
 }
